d10768e01d3ff46739eafc2cbf6ed348c5730d7d
Leandre Blanchard <leandre.blanchard@parrot.com>
2019-04-08 18:15:17 +0200

    [add] first library version

---
+++ include/arguments.h
@@ -0,0 +1,34 @@
+#ifndef _ARGUMENTS_H_
+#define _ARGUMENTS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <sys/cdefs.h>
+
+#define arg_handler(type)                                                      \
+	struct {                                                               \
+		const char *opt;                                               \
+		const char *optlong;                                           \
+		int (*handler)(type * data, const char *args[]);               \
+		const char *help;                                              \
+		const char *args_type_info;                                    \
+		bool argument_needed : 1;                                      \
+	}
+
+typedef struct options_hander_s {
+	const arg_handler(void *) * array;
+	size_t len;
+} opts_handler_t;
+
+void opts_create(opts_handler_t *opts, const void *opts_array, size_t length) __nonnull((1, 2));
+int opts_get(const opts_handler_t *opts, int ac, const char *av[], void *data) __THROW __nonnull((1, 3, 4));
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* !_ARGUMENTS_H_ */
---
+++ tests/args_tests.c
@@ -0,0 +1,44 @@
+#include <criterion/criterion.h>
+
+#include "arguments.h"
+
+#define ARRAY_LEN(array) sizeof(array) / sizeof(*array)
+
+struct options_s {
+        int iflag;
+        bool test_flag;
+};
+
+static int iflag_handler(struct options_s *options, const char *args[])
+{
+        cr_assert_str_eq(args[1], "157");
+        options->iflag = atoi(args[1]);
+        return (0);
+}
+
+static int tflag_handler(struct options_s *options, __attribute__ ((unused)) const char *args[])
+{
+        options->test_flag = true;
+        return (0);
+}
+
+static const arg_handler(struct options_s) TEST_OPTS[] = {
+        {"-i", "--index", iflag_handler, "I'm a flag to be tested", "INTEGER", true},
+        {"-t", "--test", tflag_handler, "I'm a flag to be tested", NULL, false},
+};
+
+
+
+Test(opts, success)
+{
+        static const char *av[] = {
+                "libtest", "-i", "157", "--test"
+        };
+        struct options_s options;
+        opts_handler_t handler;
+
+        opts_create(&handler, TEST_OPTS, ARRAY_LEN(TEST_OPTS));
+        cr_assert(opts_get(&handler, ARRAY_LEN(av), av, &options) == 0);
+        cr_assert_eq(options.iflag, 157);
+        cr_assert_eq(options.test_flag, true);
+}
